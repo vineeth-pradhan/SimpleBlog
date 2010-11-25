e786cfe898c393534838e3a0e0e518508b82e59f
Vineeth Pradhan <vpvineeth@idexcel.com>
2010-11-25 19:14:12 +0530

    Implemented Ajax feature for blog post comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,22 +12,26 @@ class CommentsController < CustomisedCrudController
     @comment = @blog_post.comments.build
     respond_to do |format|
       format.html
-      format.js{render :action => 'new'}
+      format.js
     end
   end
 
   def create
     #TODO: make the user_id not to be null, after creation of users and authentication
-    @blog_post = BlogPost.find(params[:blog_post_id])
-    @comment = @blog_post.comments.build(params[:comment])
-    if(@comment.save)
+    @record = BlogPost.find(params[:blog_post_id])
+    @comment = @record.comments.new(:content=>params[:content])
+    @comment.user=User.find(params[:user_id])
+    if(@comment.save!)
+      flash[:notice]="Comment successfully posted"
       respond_to do |format|
-        format.html {redirect_to @blog_post, :notice => "New comment added"}
+        #format.html {redirect_to @blog_post, :notice => "New comment added"}
+        format.js
       end
     else
       respond_to do |format|
         flash[:error] = "New comment could not be created"
         format.html {render blog_posts}
+        format.js
       end
     end
   end
