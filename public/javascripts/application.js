4a62df12e5e92e3f091c15d47fdbcd77d6c2d2ee
Vineeth Pradhan <vpvineeth@idexcel.com>
2010-11-26 18:59:29 +0530

    Javascript code clean up, moved all javascripts to application.js javascript file. And also got the flash notice Ajax feature to work after new comments are posted.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,13 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+function commentBoxSlideUp(){
+	Effect.SlideUp('new_comment',{duration: 0.3});
+	Effect.Appear('new_comment_link',{duration:0.1});	
+	return false;
+}
+
+function slideDownNewCommentBox(){
+	Effect.SlideDown('new_comment',{duration: 0.3});
+	Effect.Fade('new_comment_link',{duration: 0.5});
+    return false;
+}
